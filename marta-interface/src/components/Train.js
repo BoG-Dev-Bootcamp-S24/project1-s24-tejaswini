1421c723c2f074ca7e85b2c16ca4062c788dc18c
tejaswini-rb <tejaswini.ramkumar@gmail.com>
2024-02-25 16:05:42 -0500

    changed json parsing variables

--- marta-interface/src/components/Train.js
+++ marta-interface/src/components/Train.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Train = ({ DESTINATION, LINE_COLOR, WAITING_TIME, STATUS }) => {
-  // The props should match the actual data keys you receive from the API
-  const isDelayed = STATUS == "DELAY";
+const Train = ({ DESTINATION, DIRECTION, EVENT_TIME, HEAD_SIGN,  LINE, NEXT_ARR, STATION, TRAIN_ID, WAITING_SECONDS, WAITING_TIME
+, RESPONSETIMESTAMP, VEHICLELONGITUDE, VEHICLELATITUDE, DELAY, TRIP_ID}) => {
+  const isDelayed = DELAY != "T0S";
 
   return (
-    <div className={`train ${isDelayed ? 'DELAY' : 'ONTIME'}`} style={{ border: `1px solid ${LINE_COLOR}`, margin: '10px', padding: '10px' }}>
+    <div className={`train ${isDelayed ? 'DELAY' : 'ONTIME'}`} style={{ border: `1px solid ${LINE}`, margin: '10px', padding: '10px' }}>
       <h2>{DESTINATION}</h2>
-      <p>Line Color: {LINE_COLOR}</p>
+      <p>Line Color: {LINE}</p>
       <p>Waiting Time: {WAITING_TIME}</p>
       <p>Status: {isDelayed ? 'Delayed' : 'On Time'}</p>
     </div>
